1122835339858c7cc31816d336a179d24255ab51
TheTai <thetai.hvktqs@gmail.com>
2017-06-14 10:51:13 +0700

    init migrations

--- database/migrations/2017_06_14_015531_create_news_type_table.php
+++ database/migrations/2017_06_14_015531_create_news_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNewsTypeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,19 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news_type', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('category_id');
             $table->string('name');
-            $table->string('email');
-            $table->integer('level')->default(0);
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('slug');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
@@ -32,6 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news_type');
     }
 }
